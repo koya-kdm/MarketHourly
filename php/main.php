f919598c8ed2ca2ac7e32c839c982d677b6d0708
Koya Kodama <koya@snowsmile.net>
2015-05-09 23:00:53 +0900

    activate tweet

--- php/main.php
+++ php/main.php
@@ -24,17 +24,16 @@ date_default_timezone_set('Asia/Tokyo');
 $mm = new MarketManager();
 
 
-
 // アセット定義
 $assetsByMarket = array($mm::FX => array(0 => new Asset( 'USD',   'USDJPY=X', '円', 2, $mm::FX, false, false, null     ),
-                                           1 => new Asset( 'EUR',   'EURJPY=X', '円', 2, $mm::FX, false, false, null     ),),
+                                         1 => new Asset( 'EUR',   'EURJPY=X', '円', 2, $mm::FX, false, false, null     ),),
                         $mm::JP => array(0 => new Asset('日経',      '^N225', '円', 0, $mm::JP,  true, false, null     ),),
                         $mm::HK => array(0 => new Asset('香港',       '^HSI', 'pt', 0, $mm::HK,  true, false, null     ),),
                         $mm::SH => array(0 => new Asset('上海',  '000001.SS', 'pt', 0, $mm::SH,  true,  true, '7521596'),),
                         $mm::EU => array(0 => new Asset(  '英',  '^FTSE',     'pt', 0, $mm::EU,  true, false, null     ),
-                                           1 => new Asset(  '独',  '^GDAXI',    'pt', 0, $mm::EU,  true, false, null     ),),
+                                         1 => new Asset(  '独',  '^GDAXI',    'pt', 0, $mm::EU,  true, false, null     ),),
                         $mm::US => array(0 => new Asset('ダウ',       '^DJI', 'pt', 0, $mm::US,  true,  true, '983582' ),
-                                           1 => new Asset('ナス',      '^IXIC', 'pt', 0, $mm::US,  true, false, null     ),),
+                                         1 => new Asset('ナス',      '^IXIC', 'pt', 0, $mm::US,  true, false, null     ),),
                        );
 
 // 各時間における表示順
@@ -150,7 +149,7 @@ $tweet = createTweet($assetsByMarket);
 echo $tweet . PHP_EOL;
 
 // ツイートの投稿
-//postTweet($twitterAuth, $tweet);
+postTweet($twitterAuth, $tweet);
 
 //===============================
 // 関数
