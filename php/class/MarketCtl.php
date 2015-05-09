4fa08bd0543b0f133a0b9e22224e5efed3bbbb55
Koya Kodama <koya@snowsmile.net>
2015-05-09 22:08:45 +0900

    constant MARKET

--- php/class/MarketCtl.php
+++ php/class/MarketCtl.php
@@ -2,22 +2,38 @@
 
 class MarketCtl
 {
+  /*
+    取引時間
+    FX      :24h
+    Tokyo   : 9:00〜15:00
+    Shanghai:10:30〜16:00
+    HongKong:10:30〜16:00
+    EU      :16:00〜 0:30 (summer time)
+             17:00〜 1:30
+    NewYork :22:30〜 5:00 (summer time)
+             23:30〜 6:00
+  */
+
   // 市場
-  const FX = 'fx'; // 為替
-  const JP = 'jp'; // 日本
-  const HK = 'hk'; // 香港
-  const SH = 'sh'; // 上海
-  const EU = 'eu'; // 欧州
-  const US = 'us'; // 米国
+  const FX = 'fx'; // 為替 24h
+  const JP = 'jp'; // 日本  9:00〜15:00
+  const HK = 'hk'; // 香港 10:30〜16:00
+  const SH = 'sh'; // 上海 10:30〜16:00
+                   //      16:00〜 0:30 (summer time)
+  const EU = 'eu'; // 欧州 17:00〜 1:30
+  const US = 'us'; // 米国 22:30〜 5:00 (summer time)
+                   //      23:30〜 6:00
   
-  // ツイート時間（取引時間）
-  var $tweetHours = array(self::FX => array(0, 1, 2, 3 ,4 ,5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,),
-                          self::JP => array(                           9, 10, 11, 12, 13, 14, 15, 16,                            ),
-                          self::HK => array(                                  11, 12, 13, 14, 15, 16, 17,                        ),
-                          self::SH => array(                                  11, 12, 13, 14, 15, 16, 17,                        ),
-                          self::EU => array(0, 1,                                                 16, 17, 18, 19, 20, 21, 22, 23,),
-                          self::US => array(0, 1, 2, 3 ,4 ,5, 6, 7,                                                           23,),
-                         );
   
+  
+  /*---------------------------
+    __construct
+  -----------------------------*/
+  public function __construct()
+  {
+    
+    
+    return;
+  }
 }
 ?>
