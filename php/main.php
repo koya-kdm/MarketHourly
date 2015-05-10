88028a746bf0a7eff413b03d41f1804ebd65402d
Koya Kodama <koya@snowsmile.net>
2015-05-10 23:22:42 +0900

    bugfix

--- php/main.php
+++ php/main.php
@@ -28,15 +28,15 @@ $tweeter   = new Tweeter();   // ツイート投稿クラス
 
 // アセット定義
 $assetsByMarket
-  = array(MarketManager::FX => array(0 => new Asset( 'USD',   'USDJPY=X', '円', 2, MarketManager::FX, false, false, null     ),
-                                     1 => new Asset( 'EUR',   'EURJPY=X', '円', 2, MarketManager::FX, false, false, null     ),),
-          MarketManager::JP => array(0 => new Asset('日経',      '^N225', '円', 0, MarketManager::JP,  true, false, null     ),),
-          MarketManager::HK => array(0 => new Asset('香港',       '^HSI', 'pt', 0, MarketManager::HK,  true, false, null     ),),
-          MarketManager::SH => array(0 => new Asset('上海',  '000001.SS', 'pt', 0, MarketManager::SH,  true,  true, '7521596'),),
-          MarketManager::UK => array(0 => new Asset(  '英',      '^FTSE', 'pt', 0, MarketManager::UK,  true, false, null     ),),
-          MarketManager::GM => array(0 => new Asset(  '独',     '^GDAXI', 'pt', 0, MarketManager::GM,  true, false, null     ),),
-          MarketManager::US => array(0 => new Asset('ダウ',       '^DJI', 'pt', 0, MarketManager::US,  true,  true, '983582' ),
-                                     1 => new Asset('ナス',      '^IXIC', 'pt', 0, MarketManager::US,  true, false, null     ),),
+  = array(MarketManager::FX => array(0 => new Asset(EmojiManager::getDoller(),  'USDJPY=X', '円', 2, MarketManager::FX, false, false, null     ),
+                                     1 => new Asset(EmojiManager::getEuro(),    'EURJPY=X', '円', 2, MarketManager::FX, false, false, null     ),),
+          MarketManager::JP => array(0 => new Asset('日経',                        '^N225', '円', 0, MarketManager::JP,  true, false, null     ),),
+          MarketManager::HK => array(0 => new Asset('香港',                         '^HSI', 'pt', 0, MarketManager::HK,  true, false, null     ),),
+          MarketManager::SH => array(0 => new Asset('上海',                    '000001.SS', 'pt', 0, MarketManager::SH,  true,  true, '7521596'),),
+          MarketManager::UK => array(0 => new Asset(  '英',                        '^FTSE', 'pt', 0, MarketManager::UK,  true, false, null     ),),
+          MarketManager::GM => array(0 => new Asset(  '独',                       '^GDAXI', 'pt', 0, MarketManager::GM,  true, false, null     ),),
+          MarketManager::US => array(0 => new Asset('ダウ',                         '^DJI', 'pt', 0, MarketManager::US,  true,  true, '983582' ),
+                                     1 => new Asset('ナス',                        '^IXIC', 'pt', 0, MarketManager::US,  true, false, null     ),),
          );
 
 // アセット再定義
