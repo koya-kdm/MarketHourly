07da9dd082461f59176bf84e991ec0f1384ae7d2
Koya Kodama <koya@Koya-no-MacBook-Air.local>
2015-12-26 14:34:12 +0900

    holiday 2016

--- php/class/MarketManager.holidays.php
+++ php/class/MarketManager.holidays.php
@@ -27,6 +27,23 @@ $configHolidays = array(
                      '2015-11-23',  //  勤労感謝の日
                      '2015-12-23',  //  天皇誕生日
                      '2015-12-31',  //  休場日
+                     
+                     '2016-01-01', // New Year's Day [jp]
+                     '2016-01-11', // Coming of Age (Adults') Day
+                     '2016-02-11', // National Founding Day
+                     '2016-03-21', // Vernal Equinox OBS
+                     '2016-04-29', // Showa Day (formerly Greenery Day)
+                     '2016-05-03', // Constitution Day
+                     '2016-05-04', // Greenery Day (formerly National Holiday)
+                     '2016-05-05', // Children's Day
+                     '2016-07-18', // Marine Day
+                     '2016-08-11', // Mountain Day
+                     '2016-09-19', // Respect for the Aged Day
+                     '2016-09-22', // Autumn Equinox
+                     '2016-10-10', // Health-Sports Day
+                     '2016-11-03', // Culture Day
+                     '2016-11-23', // Labour Thanksgiving Day
+                     '2016-12-23', // Emperor's Birthday
                    ),
   
   // 香港
@@ -43,6 +60,21 @@ $configHolidays = array(
                      '2015-10-01',  //  National Day                                                建国記念日
                      '2015-10-21',  //  Chung Yeung Festival                                        重陽節
                      '2015-12-25',  //  Christmas Day                                               クリスマス
+
+                     '2016-01-01', // New Year's Day [hk]
+                     '2016-02-08', // Lunar New Year 1
+                     '2016-02-09', // Lunar New Year 2
+                     '2016-02-10', // Lunar New Year 3
+                     '2016-03-25', // Good Friday
+                     '2016-03-28', // Easter Monday
+                     '2016-04-04', // Ching Ming Festival
+                     '2016-05-02', // Labour Day OBS
+                     '2016-06-09', // Tuen Ng Day*
+                     '2016-07-01', // SAR Establishment Day
+                     '2016-09-16', // Day Following Mid-autumn Festival*
+                     '2016-10-10', // Chung Yeung Day*
+                     '2016-12-26', // Christmas Holiday
+                     '2016-12-27', // Christmas Day OBS
                    ),
   
   // 上海
@@ -61,6 +93,24 @@ $configHolidays = array(
                      '2015-10-05',  //  建国記念日
                      '2015-10-06',  //  建国記念日
                      '2015-10-07',  //  建国記念日
+
+                     '2016-01-01', // New Year's Day [sh]
+                     '2016-02-08', // Lunar New Year 1
+                     '2016-02-09', // Lunar New Year 2
+                     '2016-02-10', // Lunar New Year 3
+                     '2016-02-11', // Lunar New Year 4
+                     '2016-02-12', // Lunar New Year 5
+                     '2016-04-04', // Ching Ming Festival
+                     '2016-05-02', // Labour Day Holiday 2
+                     '2016-06-09', // Dragon Boat Festival (Tuen Ng Day)*
+                     '2016-06-10', // Dragon Boat Festival Holiday
+                     '2016-09-15', // Mid-autumn Festival*
+                     '2016-09-16', // Mid-autumn Festival Holiday
+                     '2016-10-03', // National Day 3
+                     '2016-10-04', // National Day 4
+                     '2016-10-05', // National Day 5
+                     '2016-10-06', // National Day 6
+                     '2016-10-07', // National Day 7
                    ),
   
   // イギリス
@@ -71,7 +121,16 @@ $configHolidays = array(
                      '2015-05-25', // Late May Bank Holiday
                      '2015-08-31', // Summer Bank Holiday
                      '2015-12-25', // Christmas
-                     '2015-12-28', // Boxing Day OBS),
+                     '2015-12-28', // Boxing Day OBS
+                     
+                     '2016-01-01', // New Year's Day [uk]
+                     '2016-03-25', // Good Friday
+                     '2016-03-28', // Easter Monday
+                     '2016-05-02', // Early May Bank Holiday
+                     '2016-05-30', // Late May Bank Holiday
+                     '2016-08-29', // Summer Bank Holiday
+                     '2016-12-26', // Christmas OBS
+                     '2016-12-27', // Boxing Day OBS
                     ),
   
   // ドイツ
@@ -83,6 +142,13 @@ $configHolidays = array(
                      '2015-12-24', // Christmas Eve
                      '2015-12-25', // Christmas Day
                      '2015-12-31', // New Year's Eve
+                     
+                     '2016-01-01', // New Year's Day [gm]
+                     '2016-03-25', // Good Friday
+                     '2016-03-28', // Easter Monday
+                     '2016-05-16', // Whitmonday
+                     '2016-10-03', // National Day
+                     '2016-12-26', // Christmas Holiday
                     ),
   
   // アメリカ
@@ -95,6 +161,16 @@ $configHolidays = array(
                      '2015-09-07',  //  Labor Day
                      '2015-11-26',  //  Thanksgiving Day
                      '2015-12-25',  //  Christmas
+                     
+                     '2015-01-01', // New Year's Day [us]
+                     '2015-01-19', // Martin Luther King Jr. Day
+                     '2015-02-16', // Presidents' Day
+                     '2015-04-03', // Good Friday
+                     '2015-05-25', // Memorial Day
+                     '2015-07-03', // Independence Day OBS
+                     '2015-09-07', // Labor Day
+                     '2015-11-26', // Thanksgiving
+                     '2015-12-25', // Christmas
                    ),
   );
 ?>
